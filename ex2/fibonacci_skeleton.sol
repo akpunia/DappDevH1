f49689362533e7ed7adbdb88cdf027723a9b19e6
Abhishek Punia <akpunia@berkeley.edu>
2017-09-15 10:10:22 -0700

    homework 1 complete

--- ex2/fibonacci_skeleton.sol
+++ ex2/fibonacci_skeleton.sol
@@ -1,10 +1,24 @@
 pragma solidity ^0.4.16;
 
-
 contract Fibonacci {
 	function calculate(uint position) returns (uint result) {
-		/* Add one variable to hold our greeting */
+		uint x = 0;
+        uint y = 1;
+        uint z = 1;
+
+        for (uint i = 0; i < position; i++){
+            x = y;
+            y = z;
+            z = x + y;
+        }
+
+        return x;
 	}
 
+
 	/* Add a fallback function to prevent contract payability and non-existent function calls */
+    function() {
+        throw 'This contract only calculates the Ficonacci sequence';
+    }
+
 }
